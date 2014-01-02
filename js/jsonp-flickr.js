cb58a3528d02146d3089e97c5a35e54d72ed1a6c
Olivier Scherrer <pode.fr@gmail.com>
2014-01-02 02:29:46 -0500

    add source and specs

---
+++ js/jsonp-flickr.js
@@ -0,0 +1,6 @@
+/**
+ * jsonp-flickr.js - https://github.com/podefr/jsonp-flickr
+ * Copyright(c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
+"use strict";
---
+++ test/jsonp-flickrSpec.js
@@ -0,0 +1,13 @@
+/**
+ * jsonp-flickr.js - https://github.com/podefr/jsonp-flickr
+ * Copyright(c) 2014 Olivier Scherrer <pode.fr@gmail.com>
+ * MIT Licensed
+ */
+var chai = require("chai"),
+	sinon = require("sinon"),
+	expect = chai.expect,
+	proxyquire = require("proxyquire").noCallThru();
+
+describe("GIVEN default Flickr", function() {
+
+});
